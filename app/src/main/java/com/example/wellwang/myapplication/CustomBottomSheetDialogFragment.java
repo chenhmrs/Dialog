7e771cd7b82e621f2d1bc91c76ac4c11875381c3
chenhmrs <1825609506@qq.com>
2018-01-24 14:15:56 +0800

    Bottom Sheet的两种方式

---
+++ app/src/main/java/com/example/wellwang/myapplication/CustomBottomSheetDialogFragment.java
@@ -0,0 +1,22 @@
+package com.example.wellwang.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jdsm on 2018/1/24.
+ */
+
+public class CustomBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.bottom_sheet,null);
+        return view;
+    }
+}
--- app/src/main/java/com/example/wellwang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wellwang/myapplication/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.wellwang.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -17,6 +20,9 @@ import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity implements LoginInputListener {
     ExtendsDialogFragment dialogFragment;
+    BottomSheetBehavior behavior;
+    @BindView(R.id.bottom_sheet)
+    Button mShowBottomSheet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +30,15 @@ public class MainActivity extends AppCompatActivity implements LoginInputListene
         ButterKnife.bind(this);
        dialogFragment= new ExtendsDialogFragment();
 
+        //获取到Bottom Sheet对象
+        View bottomSheet = findViewById(R.id.design_bottom_sheet);
+        behavior = BottomSheetBehavior.from(bottomSheet);
+        //默认设置为隐藏
+        behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
 
     }
 
-    @OnClick({R.id.button_dialog1, R.id.button_dialog2, R.id.button_dialog3, R.id.button_dialog4})
+    @OnClick({R.id.button_dialog1, R.id.button_dialog2, R.id.button_dialog3, R.id.button_dialog4,R.id.bottom_sheet,R.id.dialogfragment_sheet})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.button_dialog1:
@@ -60,7 +71,21 @@ public class MainActivity extends AppCompatActivity implements LoginInputListene
                 ExtendsDialogFragmentAlertDialog dialog1=new ExtendsDialogFragmentAlertDialog();
                 dialog1.show(getFragmentManager(),"dialogLogin");
                 break;
+            case R.id.bottom_sheet:
+                if (behavior.getState() == BottomSheetBehavior.STATE_HIDDEN) {
+                    behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    mShowBottomSheet.setText("hide");
+                } else {
+                    behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                    mShowBottomSheet.setText("show");
+                }
+                break;
+            case R.id.dialogfragment_sheet:
+                CustomBottomSheetDialogFragment fragment=new CustomBottomSheetDialogFragment();
+                fragment.show(getSupportFragmentManager(),"dialog");
+                break;
         }
+
     }
 
     @Override
